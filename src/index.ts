c0fdc1573790c6dfa15d3b166b9f6eae6244f839
ygm <756949055@qq.com>
2019-09-06 14:22:56 +0800

    reset dist

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,7 @@ const calculateToken = new SimpleLexer('2 + 3 * 5 / 3 * ( 4 + 6 );');
 
 console.log(assignmentToken.getCode());
 assignmentToken.dump();
-console.log('\n');
-console.log(compareToken.getCode());
+console.log('\n', compareToken.getCode());
 compareToken.dump();
-console.log('\n');
-console.log(calculateToken.getCode());
+console.log('\n', calculateToken.getCode());
 calculateToken.dump();
